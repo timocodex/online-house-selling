8ce4e24684a95e5f8c0497a82447e8e2713ec8c1
timocodex <timothy.og.siahaan@gmail.com>
2017-02-21 21:27:53 +0700

    done

--- client/public/js/ajax.js
+++ client/public/js/ajax.js
@@ -1,3 +1,5 @@
+var x
+var y
 var app = new Vue({
 
   // We want to target the div with an id of 'events'
@@ -8,8 +10,8 @@ var app = new Vue({
       address:'',
       price:'',
       image:'',
-      longitude:'',
-      latitude:''
+      longitude:0,
+      latitude:0
     },
     updated:{
       _id:'',
@@ -17,8 +19,8 @@ var app = new Vue({
       address:'',
       price:'',
       image:'',
-      longitude:'',
-      latitude:''
+      longitude:0,
+      latitude:0
     },
     houses:[]
   },
@@ -42,6 +44,26 @@ var app = new Vue({
           console.log(error);
         }
       });
+      var map = new GMaps({
+        div: '#akuPeta',
+        lat: -6.92133941054468,
+        lng: 107.6307529269252,
+        click: function(e){
+          map.removeMarkers()
+          map.addMarker({
+            lat:e.latLng.lat(),
+            lng:e.latLng.lng()
+          })
+          console.log(e.latLng.lat());
+          console.log(e.latLng.lng());
+          x = e.latLng.lat()
+          y = e.latLng.lng()
+        }
+      });
+      map.addMarker({
+        lat:-6.92133941054468,
+        lng:107.6307529269252
+      })
     },
 
   // Adds an event to the existing events array
@@ -52,7 +74,9 @@ var app = new Vue({
       data:{seller:app.house.seller,
             address:app.house.address,
             price:app.house.price,
-            image:app.house.image},
+            image:app.house.image,
+            latitude:x,
+            longitude:y},
       success: function(data) {
         app.fetchHouses()
         app.house = {
@@ -60,8 +84,8 @@ var app = new Vue({
         address:'',
         price:'',
         image:'',
-        longitude:'',
-        latitude:''};
+        longitude:0,
+        latitude:0};
       },
       error: function(error) {
         console.log(error);
@@ -75,7 +99,10 @@ var app = new Vue({
       data:{seller:app.updated.seller,
             address:app.updated.address,
             price:app.updated.price,
-            image:app.updated.image},
+            image:app.updated.image,
+            latitude:x,
+            longitude:y
+          },
       success: function(data) {
         app.fetchHouses()
       },
@@ -95,13 +122,36 @@ var app = new Vue({
           address:data.address,
           price:data.price,
           image:data.image,
+          latitude:data.latitude,
+          longitude:data.longitude
         }
-
       },
       error: function(error) {
         console.log(error);
       }
     });
+    setTimeout(function(){
+      var map = new GMaps({
+        div: '#akuPeta2',
+        lat: app.updated.latitude,
+        lng: app.updated.longitude,
+        click: function(e){
+          map.removeMarkers()
+          map.addMarker({
+            lat:e.latLng.lat(),
+            lng:e.latLng.lng()
+          })
+          console.log(e.latLng.lat());
+          console.log(e.latLng.lng());
+          x = e.latLng.lat()
+          y = e.latLng.lng()
+        }
+      });
+      map.addMarker({
+        lat:app.updated.latitude,
+        lng:app.updated.longitude
+      })
+    },1000)
   },
   deleteHouse: function(id){
     $.ajax({
